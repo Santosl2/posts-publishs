b10cd48317c60761b1a70b2c83e1f92620c98ac7
Santosl2 <mfilype2019@gmail.com>
2021-06-26 15:26:26 -0300

    Design do projeto

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,34 +1,86 @@
 <template>
-  <div class="md-layout md-gutter">
-    <div class="md-layout-item">c</div>
-    <div class="md-layout-item">f</div>
-    <div class="md-layout-item">c</div>
+  <div class="md-layout md-gutter md-alignment-center-center">
+    <div class="md-layout-item md-size-40">
+      <div class="box">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis enim ex,
+        ultricies sed ultricies in, convallis id justo. Duis vel lacinia eros.
+        Nunc et mollis erat, mattis blandit metus. Sed ullamcorper condimentum
+        lorem, vitae ullamcorper velit hendrerit ut. Praesent et lacinia nulla,
+        ac bibendum purus. Proin pulvinar et orci at placerat. Sed laoreet
+        interdum turpis, vel lobortis leo ultricies tempor. Quisque mattis nisl
+        in pharetra posuere. Vivamus massa orci, ultricies nec justo eget,
+        posuere pellentesque velit. Mauris congue quam ut imperdiet viverra.
+        Quisque a ornare sapien. Duis blandit egestas lectus, eu laoreet mauris
+        efficitur eu. Sed sodales mi vitae odio porta sollicitudin. Pellentesque
+        laoreet sagittis maximus. Cras sit amet tristique mauris. Nam consequat
+        metus libero, a pulvinar massa aliquam nec. Nullam sodales posuere eros
+        vel mattis. Praesent aliquam pellentesque augue, ut blandit justo
+        lobortis ac. Sed diam urna, ullamcorper eget massa vitae, venenatis
+        volutpat odio. Mauris efficitur feugiat efficitur. Curabitur efficitur
+        tellus eu ullamcorper fermentum. Ut sit amet nibh vel nisl dignissim
+        luctus a maximus ante. Maecenas auctor condimentum condimentum. Mauris
+        finibus tristique sagittis. Phasellus blandit consequat dapibus.
+        Vestibulum at nisl diam. Donec ac urna lorem. Pellentesque faucibus
+        velit lobortis, cursus lectus eget, tincidunt tellus. Nullam blandit
+        lacus quis justo elementum, in tempor magna dapibus. Etiam eu enim
+        ultricies, laoreet dui ac, aliquet tortor. Mauris quis sagittis elit.
+        Suspendisse a ex ultrices velit dignissim facilisis. Cras laoreet ipsum
+        in ex finibus facilisis. Duis porta elementum purus gravida ultricies.
+        Praesent vestibulum turpis vel quam lobortis, at sagittis elit luctus.
+        Maecenas quis diam ut purus euismod euismod et ac mauris. Aenean
+        fringilla ullamcorper eros a gravida. Donec ac nisl rhoncus, faucibus
+        elit in, rutrum nisi. Ut non arcu ut nibh gravida venenatis. Mauris
+        tincidunt nibh enim, vel blandit arcu rhoncus sed. Duis luctus arcu eget
+        velit tincidunt, non consectetur purus feugiat. Nulla euismod imperdiet
+        tellus non bibendum. Cras posuere ullamcorper purus, a rhoncus nisi
+        imperdiet ac. Ut nec elit massa. Quisque dignissim lacus dolor, sed
+        lobortis quam condimentum nec. Nullam id ante faucibus, vulputate orci
+        mattis, viverra mi. Nam sed purus ac nunc malesuada lobortis vel et leo.
+        Mauris sed maximus sapien. Nulla scelerisque ipsum ac enim ullamcorper,
+        at luctus quam imperdiet. Praesent venenatis risus quis orci rutrum, non
+        ultrices justo euismod. Sed suscipit libero convallis diam dignissim,
+        vestibulum fringilla ex ultricies. Vivamus finibus placerat leo eget
+        gravida. Vivamus iaculis, sapien sed aliquet dapibus, metus nulla
+        viverra eros, non aliquet massa justo eget ex. Maecenas vestibulum
+        sagittis ornare. Duis venenatis erat in erat porttitor bibendum. Nam
+        erat orci, lobortis vitae tincidunt at, condimentum non enim. Vivamus
+        sit amet odio nec ligula cursus euismod nec in augue. Aenean aliquet
+        purus varius sagittis molestie.
+
+        <div class="box-footer">
+          <md-button class="md-icon-button md-dense">
+            <md-icon class="md-accent">thumb_up</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button md-dense md-primary">
+            <md-icon>comment</md-icon>
+          </md-button>
+        </div>
+      </div>
+      <div class="box">hello</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  name: "Main",
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.white {
+  color: white !important; 
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.box {
+  background: #131212;
+  min-width: 75px;
+  padding: 10px;
+  margin: 2px;
+  border-radius: 7px;
+  color: white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.box > .box-footer {
+  margin-top: 30px;
+  margin-bottom: 20px;
 }
 </style>
