2e787f486e4a28cd2432b92d7acbf0728a3bebe3
Santosl2 <mfilype2019@gmail.com>
2021-06-26 17:27:22 -0300

    Adicionado Post Footer

---
+++ src/components/Post/Post-Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <md-button v-on:click="toggleLiked" class="md-icon-button md-dense">
+      <md-icon :class="!isLiked ? 'white' : 'md-accent'">thumb_up</md-icon>
+    </md-button>
+    {{ textLiked }}
+  </div>
+</template>
+<style>
+.white {
+  color: white !important;
+}
+</style>
+<script>
+import { LANGUAGE } from "../../resources/constants.js";
+
+export default {
+  name: "PostFooter",
+  props: {
+    postId: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data: () => ({
+    isLiked: false,
+  }),
+  methods: {
+    toggleLiked() {
+      this.isLiked = !this.isLiked;
+    },
+  },
+  computed: {
+    textLiked: function () {
+      return !this.isLiked ? LANGUAGE.FIRST_LIKE : LANGUAGE.LIKE;
+    },
+  },
+};
+</script>
